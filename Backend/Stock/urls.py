cb4f0129c0fadf6019f34d7a072322860df17b70
kushal <kushal@Debian.ip6.wlink.com.np>
2024-03-12 21:05:27 +0545

    Downloading Data view

--- Backend/Stock/urls.py
+++ Backend/Stock/urls.py
@@ -1,11 +1,13 @@
 
 from django.urls import path
 
-from .views import index,scrape_news,upload_file,scrape_and_predict
+from .views import index,scrape_news,upload_file,download_data
 
 urlpatterns = [ 
     path ('',index, name='index'),
     path('upload/', upload_file, name='predict_stock'),
     path ('news/', scrape_news, name='news'),
-    path ('predict/',scrape_and_predict,name='scrape_and_predict')
+    path ('download/',download_data, name='download_data')
+    
+  
 ]
